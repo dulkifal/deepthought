4f67a80788a216ce135b9dcaf67731457a648b58
dulkifal <dulkifilibacker@gmail.com>
2023-03-20 09:45:52 +0530

    threads

---
+++ index.js
@@ -0,0 +1,9 @@
+// toggle down arrow on click
+ 
+function toggleDownArrow() {
+  var downArrow = document.getElementById('arrow');
+  
+  downArrow.classList.toggle('up');
+}
+
+console.log("index.js loaded");
